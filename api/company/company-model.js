7720c5f36820058b0e663aec04a915fae0a7f14f
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-20 17:56:21 -0500

    basic crud complete

--- api/company/company-model.js
+++ api/company/company-model.js
@@ -1,9 +1,41 @@
 const db = require('../../data/dbConfig.js');
 
 module.exports = {
-    getJobs
+    create,
+    getJobs,
+    getById,
+    update,
+    remove
 }
+
+function create(job) {
+    return db('company')
+      .insert(job)
+      .then(ids => {
+        return getById(ids[0]);
+      });
+  }
+
 
 function getJobs() {
     return db('company')
 }
+
+function getById(id) {
+    return db('company')
+      .where({ id })
+      .first();
+  }
+
+  function update(id, changes) {
+    return db('company')
+      .where({ id })
+      .update(changes)
+      .then(ids => ({ id: ids[0], changes}));
+  }
+
+  function remove(id) {
+    return db('company')
+      .where({ id })
+      .delete()
+  }
--- api/company/company-router.js
+++ api/company/company-router.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const Companies = require('./company-model');
+const Jobs = require('./company-model');
 
 router.get('/', (req, res) => {
-    Companies.getJobs()
-        .then(companies => {
-            res.status(200).json(companies)
+    Jobs.getJobs()
+        .then(jobs => {
+            res.status(200).json(jobs)
         })
         .catch(err => {
             res.status(400).json(err)
         })
+})
+
+router.post('/', (req, res) => {
+    Jobs.create(req.body).then(job => {
+        res.status(201).json(job)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    Jobs.update(id, req.body).then(newRecord => {
+        res.status(200).json(newRecord)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Jobs.remove(id).then(deleted => {
+        res.status(200).json({ deleted: `job of id ${id}`})
+    })
 })
 
 module.exports = router;
