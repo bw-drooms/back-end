74cd7744c8745b158041f8d3b0de30611a9032c1
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-20 15:54:52 -0500

    added env port variable for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require('./api/server.js');
 
-const PORT = 4001;
+const dbPort = process.env.DB_PORT;
+
+server.use(express.json());
+
+const PORT = process.env.PORT || dbPort || 4000;
 
 server.listen(PORT, () => {
     console.log(`!!! Server is listening on port ${PORT} !!!`)
