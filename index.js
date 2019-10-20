a4ca2bbff12a815071153543098721e80cb50482
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-20 16:03:17 -0500

    dotenv

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require('./api/server.js');
 
 const dbPort = process.env.DB_PORT;
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || dbPort || 4000;
 
 server.listen(PORT, () => {
