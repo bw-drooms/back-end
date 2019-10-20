59d0891df2df0648d07b889c3ac39aef37bafaa6
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-20 15:58:38 -0500

    removed redundancy

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const server = require('./api/server.js');
 
 const dbPort = process.env.DB_PORT;
 
-server.use(express.json());
-
 const PORT = process.env.PORT || dbPort || 4000;
 
 server.listen(PORT, () => {
