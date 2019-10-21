785eab590d440f070ad6354cae8df4a86d9035e6
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-21 17:33:26 -0500

    added authorization  and restricted middleware

--- api/server.js
+++ api/server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
+const usersRouter = require('./users/user-router.js');
 const seekerRouter = require('./jobseeker/jobseeker-router.js');
 const jobsRouter = require('./jobs/jobs-router.js');
 const companiesRouter = require('./company/company-router.js');
+const restricted = require('../auth/restricted-middleware.js');
 
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-server.use('/api/jobseekers', seekerRouter)
+server.use('/api', usersRouter)
+server.use('/api/jobseekers', restricted, seekerRouter)
 server.use('/api/jobs', jobsRouter)
-server.use('/api/companies', companiesRouter)
+server.use('/api/companies', restricted, companiesRouter)
 
 server.get('/', () => {
     console.log('welcome to droom')
