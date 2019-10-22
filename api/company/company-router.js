1f8d4abbb2f2f9d0488c8da94b4f591a914ee703
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-21 22:19:49 -0500

    crud for companies and jobseekers

--- api/company/company-router.js
+++ api/company/company-router.js
@@ -3,18 +3,40 @@ const router = express.Router();
 
 const Companies = require('./company-model');
 
+/**
+*@api {post} /api/companies
+*@apiName PostCompanies
+*@apiGroup Companies
+**/
+
 router.post('/', (req, res) => {
     Companies.create(req.body)
-        .then(job => {
-            res.status(201).json(job)
+        .then(company => {
+            res.status(201).json(company)
+    })
+    .catch(err => {
+        res.status(400).json(err)
     })
 })
+
+/**
+*@api {get} /api/companies
+*@apiName GetCompanies
+*@apiGroup Companies
+**/
 
 router.get('/', (req, res) => {
     Companies.get().then(companies => {
         res.status(200).json(companies)
     })
 })
+
+/**
+*@api {get} /api/companies/:company_id
+*@apiName GetCompaniesJobs
+*@apiGroup Companies
+* @apiParam {Number} id company's unique ID.
+**/
 
 router.get('/:company_id', (req, res) => {
     const id = req.params.company_id;
@@ -23,6 +45,41 @@ router.get('/:company_id', (req, res) => {
         res.status(200).json(company)
     })
 })
+
+/**
+*@api {put} /api/companies/:company_id
+*@apiName PutCompanies
+*@apiGroup Companies
+*@apiParam {Number} id company's unique ID.
+**/
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    Companies.update(id, changes).then(newRecord => {
+        res.status(200).json(newRecord)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+/**
+*@api {delete} /api/companies/:company_id
+*@apiName DeleteCompanies
+*@apiGroup Companies
+**/
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Jobseekers.remove(id).then(deleted => {
+        res.status(200).json({ deleted: `member of id ${id}`})
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
 
 
 module.exports = router;
--- api/jobseeker/jobseeker-router.js
+++ api/jobseeker/jobseeker-router.js
@@ -3,14 +3,11 @@ const router = express.Router();
 
 const Jobseekers = require('./jobseeker-model.js');
 
-router.get('/', (req, res) => {
-    Jobseekers.get().then(seekers => {
-            res.status(200).json(seekers)
-        })
-        .catch(err => {
-            res.status(400).json(err)
-        })
-})
+/**
+*@api {post} /api/jobseekers
+*@apiName PostJobseekers
+*@apiGroup Jobseekers
+**/
 
 router.post('/', (req, res) => {
     Jobseekers.create(req.body).then(seeker => {
@@ -21,17 +18,52 @@ router.post('/', (req, res) => {
     })
 })
 
+/**
+*@api {get} /api/jobseekers
+*@apiName GetJobseekers
+*@apiGroup Jobseekers
+**/
+
+router.get('/', (req, res) => {
+    Jobseekers.get().then(seekers => {
+            res.status(200).json(seekers)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
+
+/**
+*@api {put} /api/jobseekers/:jobseeker_id
+*@apiName PutJobseekers
+*@apiGroup Jobseekers
+*@apiParam {Number} id jobseeker's unique ID.
+**/
+
 router.put('/:id', (req, res) => {
     const id = req.params.id;
-    Jobseekers.update(id, req.body).then(newRecord => {
+    const changes = req.body;
+    Jobseekers.update(id, changes).then(newRecord => {
         res.status(200).json(newRecord)
     })
+    .catch(err => {
+        res.status(400).json(err)
+    })
 })
+
+/**
+*@api {delete} /api/jobseekers/:jobseeker_id
+*@apiName DeleteJobseekers
+*@apiGroup Jobseekers
+**/
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id
     Jobseekers.remove(id).then(deleted => {
         res.status(200).json({ deleted: `member of id ${id}`})
+    })
+    .catch(err => {
+        res.status(400).json(err)
     })
 })
 
