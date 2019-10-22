3a2cc8e3f6d8483cc1b5117e73b03f04f28c82e3
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-22 18:14:15 -0500

    added email property to user

--- api/jobseeker/jobseeker-router.js
+++ api/jobseeker/jobseeker-router.js
@@ -1,58 +1,58 @@
 const express = require('express');
 const router = express.Router();
-
+const withCatch = require('../../utils/withCatch.js');
+const isEmptyObj = require('../../utils/isEmptyObj.js');
 const Jobseekers = require('./jobseeker-model.js');
 
+
 /**
-*@api {post} /api/jobseekers
+*@api {post} /jobseekers
 *@apiName PostJobseekers
 *@apiGroup Jobseekers
 **/
 
-router.post('/', (req, res) => {
-    Jobseekers.create(req.body).then(seeker => {
-        res.status(201).json(seeker)
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
+router.post('/', async (req, res) => {
+   const [err, jobseekers] = await withCatch (Jobseekers.create(req.body))
+
+   if (err) res.status(500).json(err)
+   else res.status(201).json(jobseekers)
 })
 
 /**
-*@api {get} /api/jobseekers
+*@api {get} /jobseekers
 *@apiName GetJobseekers
 *@apiGroup Jobseekers
 **/
 
-router.get('/', (req, res) => {
-    Jobseekers.get().then(seekers => {
-            res.status(200).json(seekers)
-        })
-        .catch(err => {
-            res.status(400).json(err)
-        })
+router.get('/', async (req, res) => {
+    const [err, jobseekers] = await withCatch (Jobseekers.get())
+
+    if (err) res.status(500).json(err)
+    else if (err || isEmptyObj(jobseekers)) res.status(404).json({ error: "There are no jobseekers available yet."})
+    else res.status(200).json(jobseekers)
 })
 
 /**
-*@api {put} /api/jobseekers/:jobseeker_id
+*@api {put} /jobseekers/:jobseeker_id
 *@apiName PutJobseekers
 *@apiGroup Jobseekers
 *@apiParam {Number} id jobseeker's unique ID.
 **/
 
-router.put('/:id', (req, res) => {
-    const id = req.params.id;
+router.put('/:id', async (req, res) => {
+
     const changes = req.body;
-    Jobseekers.update(id, changes).then(newRecord => {
-        res.status(200).json(newRecord)
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
+
+    const [err, jobseeker] = await withCatch (Jobseekers.update(req.params.id, changes))
+    console.log(err, jobseeker)
+    if (err) res.status(500).json({message: "there was a problem with the request", err})
+    else if (err || isEmptyObj(jobseeker)) res.status(404).json({ error: "There is no jobseeker available yet."})
+    else res.status(200).json(jobseeker)
+
 })
 
 /**
-*@api {delete} /api/jobseekers/:jobseeker_id
+*@api {delete} /jobseekers/:jobseeker_id
 *@apiName DeleteJobseekers
 *@apiGroup Jobseekers
 **/
